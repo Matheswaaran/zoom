668ffdc736dd2dcb0931fb9c5e6b9acf724e262d
matheswaaran <matheswaarans@gmail.com>
2017-02-28 08:03:04 +0530

    Initial

---
+++ app/src/main/java/com/example/hp/rockin/MainActivity.java
@@ -0,0 +1,40 @@
+package com.example.hp.rockin;
+
+import android.graphics.Matrix;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.MotionEvent;
+import android.view.ScaleGestureDetector;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+    ImageView imageView;
+    Matrix matrix = new Matrix();
+    float scale = 1f;
+    ScaleGestureDetector SGD;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        imageView = (ImageView)findViewById(R.id.imageview);
+        SGD = new ScaleGestureDetector(this, new ScaleListener());
+    }
+
+    private class ScaleListener extends ScaleGestureDetector.SimpleOnScaleGestureListener{
+        @Override
+        public boolean onScale(ScaleGestureDetector detector) {
+            scale = scale * detector.getScaleFactor();
+            scale = Math.min(2f, Math.min(scale, 5f));
+            matrix.setScale(scale, scale);
+            imageView.setImageMatrix(matrix);
+            return true;
+        }
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        SGD.onTouchEvent(event);
+        return true;
+    }
+}
